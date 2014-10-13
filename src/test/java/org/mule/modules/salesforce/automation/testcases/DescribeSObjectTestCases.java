639e6803edb2305eaf5d07e9c93e8be4b1d1bb7e
Cristian Ambrozie <cristian.ambrozie@softvision.ro>
2014-10-13 15:12:31 +0300

    SCC-87 Extend functional tests for describe Sobject operation

--- src/test/java/org/mule/modules/salesforce/automation/testcases/DescribeSObjectTestCases.java
+++ src/test/java/org/mule/modules/salesforce/automation/testcases/DescribeSObjectTestCases.java
@@ -13,53 +13,67 @@ package org.mule.modules.salesforce.automation.testcases;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mule.modules.salesforce.automation.RegressionTests;
 import org.mule.modules.salesforce.automation.SalesforceTestParent;
 import org.mule.modules.tests.ConnectorTestUtils;
 
+import com.sforce.soap.partner.DescribeGlobalResult;
+import com.sforce.soap.partner.DescribeGlobalSObjectResult;
 import com.sforce.soap.partner.DescribeSObjectResult;
 
-
 public class DescribeSObjectTestCases extends SalesforceTestParent {
-	
-    @Category({RegressionTests.class})
-	@Test
-	public void testDescribeSObjectForAccount() {
-    	
-		try {
-			
-			loadTestRunMessage("describeSObjectAccountTestData");
-			
-			DescribeSObjectResult describeSObjectResult = runFlowAndGetPayload("describe-sobject");
-			String sObjectName = describeSObjectResult.getName();
 
-	        assertTrue(sObjectName.equals(getTestRunMessageValue("type")));
+	Set<String> salesforceTypes;
+
+	@Before
+	public void setUp() throws Exception {
+
+		salesforceTypes = new HashSet<String>();
+
+		try {
+
+			initializeTestRunMessage("describeGlobalTestData");
+
+			DescribeGlobalResult describeGlobalResult = runFlowAndGetPayload("describe-global");
+			DescribeGlobalSObjectResult[] sObjects = describeGlobalResult
+					.getSobjects();
+
+			for (int index = 0; index < sObjects.length; index++) {
+				salesforceTypes.add(sObjects[index].getName());
+			}
 
 		} catch (Exception e) {
 			fail(ConnectorTestUtils.getStackTrace(e));
 		}
-     
+
 	}
-    
-    @Category({RegressionTests.class})
-   	@Test
-   	public void testDescribeSObjectForUser() {
-       	
-   		try {
-   			
-   			loadTestRunMessage("describeSObjectUserTestData");
-   			
-   			DescribeSObjectResult describeSObjectResult = runFlowAndGetPayload("describe-sobject");
-   			String sObjectName = describeSObjectResult.getName();
 
-   	        assertTrue(sObjectName.equals(getTestRunMessageValue("type")));
+	@Category({ RegressionTests.class })
+	@Test
+	public void testDescribeSObjectForAllSalesforceTypes() {
+		try {
+			Map<String, Object> testData = new HashMap<String, Object>();
+			for (String salesforceType : salesforceTypes) {
+				testData.put("type", salesforceType);
+				initializeTestRunMessage(testData);
+				DescribeSObjectResult describeSObjectResult = runFlowAndGetPayload("describe-sobject");
+				String sObjectName = describeSObjectResult.getName();
 
-   		} catch (Exception e) {
-   			fail(ConnectorTestUtils.getStackTrace(e));
-   		}
-        
-   	}
+				assertTrue(sObjectName.equals(getTestRunMessageValue("type")));
+			}
+			
+		} catch (Exception e) {
+			fail(ConnectorTestUtils.getStackTrace(e));
+		}
+
+	}
 
 }
